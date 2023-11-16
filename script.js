8d887657c6cc6691dcbcf51234f75037cf6120ac
AmithAKulk <95955499+AmithAKulk@users.noreply.github.com>
2023-11-16 18:52:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 //your JS code here. If required.
- function incrementCounter() {
-            // Get the current counter value
-            const counterElement = document.getElementById('counter');
-            let currentValue = parseInt(counterElement.innerText);
+ let x=1;
+let co=document.getElementById("counter");
+var counterValue = 0;
+ var incrementBtn = document.getElementById('incrementBtn');
+ incrementBtn.addEventListener('click', function() {
+            alert(co.innerText);
+      
+            
+	        co.innerText=++counterValue;
+    
+        });
 
-            // Display the un-incremented value in an alert
-            alert("Un-incremented value: " + currentValue);
 
-            // Increment the counter value
-            currentValue++;
-
-            // Update the counter display
-            counterElement.innerText = currentValue;
-        }
+// function f1(){
+// 	alert(co.innerText);
+// 	co.innerText=x++;
+	
+// }
